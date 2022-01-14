19367883ad681c7e350e103a0b4175f2902c5be2
Grips001 <jdvise001@gmail.com>
2022-01-13 19:17:51 -0500

    Update True-Random.psm1
    updated top boundary of max result

--- True-Random/True-Random.psm1
+++ True-Random/True-Random.psm1
@@ -1,9 +1,10 @@
 ﻿function True-Random{
 param([int]$min,[int]$max)
+$max1=$max+1
 (invoke-restmethod ndbc.noaa.gov/data/realtime2) -match '<a href=" ?(.*)</a>' | Out-Null;$raw=$Matches[1];$dataSet=Get-Random (([regex]::Matches($raw, '(?<="\>).+?(?=\<)').Value) -like "*.adcp")
 $seedLong = (Invoke-RestMethod ndbc.noaa.gov/data/realtime2/$dataSet) -replace '\D+'
 $seedRan = Get-Random -Maximum ($seedLong.Length+1) -Minimum 11
 $seedStart = $seedRan - 8
 $seed = -join $seedLong[$seedStart..$seedRan]
-Get-Random -Maximum $max -Minimum $min -SetSeed $seed
+Get-Random -Maximum $max1 -Minimum $min -SetSeed $seed
 }
