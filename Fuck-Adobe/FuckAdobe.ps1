de27597d112de9f10f1f9c2d78ab6b8f0bdf8b8a
Grips001 <jdvise001@gmail.com>
2022-01-17 19:05:00 -0500

    Update FuckAdobe.ps1
    removed pause

--- Fuck-Adobe/FuckAdobe.ps1
+++ Fuck-Adobe/FuckAdobe.ps1
@@ -1,3 +1,2 @@
 get-service | ?{$_.DisplayName -like "*Adobe*"} | Stop-Service -Force
 get-process | ?{$_.ProcessName -Like "*Adobe*" -or $_.ProcessName -Like "*CC*" -or $_.ProcessName -Like "*Creative*" -or $_.ProcessName -Like "*CoreSync*"} | Stop-Process -Force
-pause
